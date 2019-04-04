b4771c5b801164d288a06da4bed8bb6b06eea309
Ricardo Paul <kingupper0@gmail.com>
2019-04-04 19:32:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,8 +11,11 @@ end
 def valid_move?(board, index)
   if index.between?(0,8)
     if !position_taken?(board, index)
-      true
+      return true
     end
+  else
+    return false
+  end
 end 
 
 
